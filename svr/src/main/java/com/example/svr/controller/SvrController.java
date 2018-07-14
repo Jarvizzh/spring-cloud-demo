b4b9e698d4c344270dfe5ff6b71292f279762d0d
jarvizzhang <jarvizzhang@tencent.com>
2018-07-14 22:28:05 +0800

    update

--- svr/src/main/java/com/example/svr/controller/SvrController.java
+++ svr/src/main/java/com/example/svr/controller/SvrController.java
@@ -1,19 +1,18 @@
-package com.example.server.controller;
+package com.example.svr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Author: jarvizzhang
- * Date: 2018/7/13
+ * Date: 2018/7/14
  */
 @RestController
-public class ServerController {
+public class SvrController {
 
     @GetMapping("/svr")
-    public String helloSvr() {
-        return "hello svr";
+    public String test(){
+        return "this is a server.";
     }
-
 
 }
